cc05994525e15448f52b9f6649acc5e86d1dfcea
Dennis Oberhoff <dennisoberhoff@gmail.com>
2015-12-13 21:53:09 +0100

    Removed default script for Cocoapods

--- CocoaPods.lbaction/Contents/Scripts/default.py
+++ CocoaPods.lbaction/Contents/Scripts/default.py
@@ -1,17 +1,25 @@
-#!/usr/bin/python
-#
-# Copyright (c) 2014 Objective Development
-# http://www.obdev.at/
-
-import urllib
+import json
 import sys
-import webbrowser
+import urllib2
+import urllib
 
-base_url = 'http://cocoapods.org/?q=%s'
+base_url = 'http://search.cocoapods.org/api/pods?%s'
 arguments = ' '.join(sys.argv[1:])
+scriptOutput = []
 
 def search(search_string):
-	url = base_url % urllib.quote(search_string)
-	webbrowser.open(url)
+
+	url = base_url % urllib.urlencode({'query': search_string, 'amount': 10})
+	q = urllib2.Request(url)
+	q.add_header('Accept', 'application/vnd.cocoapods.org+flat.hash.json; version=1')
+	q.add_header('User-Agent', 'LaunchBar')
+
+	string_results = urllib2.urlopen(q).read()
+	json_results = json.loads(string_results)
+   
+	for item in json_results:
+		scriptOutput.append({'title' : item["id"], 'subtitle': item["summary"], 'url': item["link"], 'icon' : 'Cocoapods'})
 
 search(arguments)
+
+print json.dumps(scriptOutput)
