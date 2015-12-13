3c0ba81ff24f8ebbc0c30cbe7a78494b53b2cb7d
Dennis Oberhoff <dennisoberhoff@gmail.com>
2015-12-13 14:55:11 +0100

    Updated Cocoapods Script

--- CocoaPods.lbaction/Contents/Scripts/suggestions.py
+++ CocoaPods.lbaction/Contents/Scripts/suggestions.py
@@ -1,18 +1,24 @@
 import json
 import sys
+import urllib2
 import urllib
 
-base_url = 'http://search.cocoapods.org/api/v1/pods.flat.hash.json?%s'
+base_url = 'http://search.cocoapods.org/api/pods?%s'
 arguments = ' '.join(sys.argv[1:])
 scriptOutput = []
 
 def search(search_string):
-    url = base_url % urllib.urlencode({'query': search_string, 'amount': 10})
-    string_results = urllib.urlopen(url).read()
-    json_results = json.loads(string_results)
+
+	url = base_url % urllib.urlencode({'query': search_string, 'amount': 10})
+	q = urllib2.Request(url)
+	q.add_header('Accept', 'application/vnd.cocoapods.org+flat.hash.json; version=1')
+	q.add_header('User-Agent', 'LaunchBar')
+
+	string_results = urllib2.urlopen(q).read()
+	json_results = json.loads(string_results)
    
-    for item in json_results:
-        scriptOutput.append({'title' : item["id"], 'subtitle': item["summary"], 'icon' : 'Cocoapods'})
+	for item in json_results:
+		scriptOutput.append({'title' : item["id"], 'subtitle': item["summary"], 'icon' : 'Cocoapods'})
 
 search(arguments)
 
